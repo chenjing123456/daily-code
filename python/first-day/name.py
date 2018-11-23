e467e727c8127a3aa7d459cd23fa4aeb7342b831
三日 <1474189135@qq.com>
2018-11-23 16:55:55 +0800

    add python

---
+++ python/first-day/name.py
@@ -0,0 +1,5 @@
+name = input('please enter your name:');
+print('hello',name);
+
+num = 1024*768;
+print('1024*768 = ',num);
---
+++ python/first-day/tab.py
@@ -0,0 +1,6 @@
+# 练习
+a = 100
+if a >= 0:
+    print(a)
+else:
+    print(-a)
